4d6fe0c5f3100c4960c94f26b13a3cb09e464205
leandro3742 <leandro.marrero@outlook.com>
2022-12-11 19:18:22 -0300

    add product class

--- src/api/products/product.dto.ts
+++ src/api/products/product.dto.ts
@@ -1,8 +1,11 @@
 import { IsNumber, IsNumberString, IsString } from 'class-validator';
 
 export class CreateProductDto {
+  @IsString()
   public name: string;
+  @IsString()
   public stock: number;
+  @IsString()
   public price: number;
 
 }
